9634a73a77c4d162658fff3a3b019b732cd6e5a8
Roman Alyokhin <roman.alyokhin@gmail.com>
2017-12-15 11:32:58 +0200

    Лямбда-выражения для групп, контактов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class GroupCreationTests extends TestBase {
 
     List<GroupData> before = app.getGroupHelper().getGroupList();
 
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     //int before = app.getGroupHelper().getGroupCount();
 
     app.getGroupHelper().createGroup(group);
@@ -26,13 +27,7 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() +1);
 
 
-    int max = 0;
-    for (GroupData g : after){
-      if (g.getId() > max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
